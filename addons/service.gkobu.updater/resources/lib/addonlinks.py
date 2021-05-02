655acc2fb45cda0d72a14b27e51304323663b3c1
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2021-05-02 22:53:12 +0300

    Add files via upload

--- addons/service.gkobu.updater/resources/lib/addonlinks.py
+++ addons/service.gkobu.updater/resources/lib/addonlinks.py
@@ -1,9 +1,5 @@
 ######################'addonid'############################'repoid'#############
 ADDONS_REPOS = [('service.gkobu.updater',               'repository.gkobu'),
-                ('inputstream.adaptive',                'b6a50484-93a0-4afb-a01c-8d17e059feda'),
-                ('inputstream.rtmp',                    'repository.xbmc.org'),
-                ('vfs.libarchive',                      'repository.xbmc.org'),
-                ('vfs.rar',                             'repository.xbmc.org'),
                 ('plugin.program.downloader',           'repository.gkobu'),
                 ('plugin.program.autocompletion',       'repository.gkobu')
                  ]
