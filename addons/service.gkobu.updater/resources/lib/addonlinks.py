0b42f525aee1213d2385665eab9e8a1daccd05c1
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2021-05-06 23:47:22 +0300

    Delete addons/service.gkobu.updater directory

--- addons/service.gkobu.updater/resources/lib/addonlinks.py
+++
@@ -1,7 +0,0 @@
-
-ADDONS_REPOS = [('service.gkobu.updater',               'repository.gkobu'),
-                ('plugin.program.downloader',           'repository.gkobu'),
-                ('plugin.program.autocompletion',       'repository.gkobu')
-                 ]
-
-URLS = ['http://gknwizard.eu/repo/Builds/GKoBu/GKoBuM/gkobu_rescue.zip', 'http://gknwizard.eu/repo/Builds/GKoBu/GKoBuM/gkobu_autoexec.zip']
