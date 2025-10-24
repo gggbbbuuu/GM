931e98b4b38f92cecad055b125f77b667ecf3136
gggbbbuuu_git <57154026+gggbbbuuu@users.noreply.github.com>
2025-10-24 20:01:52 +0300

    MicroJen 3.8.67, ResolveURL 5.1.184

--- addons/script.module.resolveurl/lib/resolveurl/plugins/fastream.py
+++ addons/script.module.resolveurl/lib/resolveurl/plugins/fastream.py
@@ -1,6 +1,6 @@
 """
     Plugin for ResolveURL
-    Copyright (C) 2023 bassemhelal18
+    Copyright (C) 2025 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,23 +16,17 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 
-class AnafastResolver(ResolveGeneric):
-    name = 'Anafast'
-    domains = ['anafast.online', 'anafasts.com']
-    pattern = r'(?://|\.)(anafasts?\.(?:online|com))/(?:embed-)?([0-9a-zA-Z]+)'
+class FastreamResolver(ResolveGeneric):
+    name = 'Fastream'
+    domains = ['fastream.to']
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
             self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[(?:{\s*file:)?\s*"(?P<url>[^"]+)'''],
-            generic_patterns=False,
-            referer=False
+            patterns=[r'sources:\s*\[{\s*file:\s*"(?P<url>[^"]+)'],
+            generic_patterns=False
         )
-
-    def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
